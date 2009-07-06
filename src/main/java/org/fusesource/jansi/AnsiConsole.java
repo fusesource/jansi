d13e053242fca5ff1086989122b6bb4c4d74939c
Hiram Chirino <hiram@hiramchirino.com>
2009-07-06 13:32:41 -0400

    added docs

--- src/main/java/org/fusesource/jansi/AnsiConsole.java
+++ src/main/java/org/fusesource/jansi/AnsiConsole.java
@@ -1,3 +1,10 @@
+/**
+ *  Copyright (C) 2008 Progress Software, Inc. All rights reserved.
+ *  http://fusesource.com
+ *
+ *  The software in this package is published under the terms of the AGPL license
+ *  a copy of which has been included with this distribution in the license.txt file.
+ */
 package org.fusesource.jansi;
 
 import java.io.OutputStream;
