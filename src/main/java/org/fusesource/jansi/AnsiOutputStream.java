7b1a3ccbafce5c2a2c4433343a681f49633c0e13
Hiram Chirino <hiram@hiramchirino.com>
2009-07-07 00:04:35 -0400

    Switching to ASL 2.0 License

--- src/main/java/org/fusesource/jansi/AnsiOutputStream.java
+++ src/main/java/org/fusesource/jansi/AnsiOutputStream.java
@@ -1,10 +1,20 @@
 /**
- *  Copyright (C) 2008 Progress Software, Inc. All rights reserved.
- *  http://fusesource.com
+ * Copyright (C) 2009, Progress Software Corporation and/or its 
+ * subsidiaries or affiliates.  All rights reserved.
  *
- *  The software in this package is published under the terms of the AGPL license
- *  a copy of which has been included with this distribution in the license.txt file.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.fusesource.jansi;
 
 import java.io.FilterOutputStream;
