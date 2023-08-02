3789a9c751ed21ab2463ae5cdbe643ac853eaa7e
Arthur Chan <arthur.chan@adalogics.com>
2023-08-02 20:01:56 +0000

    Fix number wrap around bug
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- src/main/java/org/fusesource/jansi/Ansi.java
+++ src/main/java/org/fusesource/jansi/Ansi.java
@@ -706,10 +706,16 @@ public class Ansi implements Appendable {
     }
 
     public Ansi scrollUp(final int rows) {
+        if (rows == Integer.MIN_VALUE) {
+            return sctollDown(Integer.MAX_VALUE);
+        }
         return rows > 0 ? appendEscapeSequence('S', rows) : rows < 0 ? scrollDown(-rows) : this;
     }
 
     public Ansi scrollDown(final int rows) {
+        if (rows == Integer.MIN_VALUE) {
+            return sctollUp(Integer.MAX_VALUE);
+        }
         return rows > 0 ? appendEscapeSequence('T', rows) : rows < 0 ? scrollUp(-rows) : this;
     }
 
