af986650b4630c0ac6f575c40b0466142188e423
Glavo <zjx001202@gmail.com>
2023-10-02 22:00:46 +0800

    update

--- src/test/java/org/fusesource/jansi/AnsiTest.java
+++ src/test/java/org/fusesource/jansi/AnsiTest.java
@@ -20,7 +20,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tests for the {@link Ansi} class.
