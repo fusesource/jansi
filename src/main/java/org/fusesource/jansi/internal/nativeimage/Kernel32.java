74b8320472116258974eb400f3a24ab2c9faf263
Glavo <zjx001202@gmail.com>
2023-10-02 23:09:16 +0800

    fix build

--- src/main/java/org/fusesource/jansi/internal/nativeimage/Kernel32.java
+++ src/main/java/org/fusesource/jansi/internal/nativeimage/Kernel32.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2009-2023 the original author(s).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.fusesource.jansi.internal.nativeimage;
 
 import org.graalvm.nativeimage.Platform;
@@ -37,6 +52,5 @@ final class Kernel32 {
     @CFunction(transition = CFunction.Transition.NO_TRANSITION)
     static native int SetConsoleTitleW(CCharPointer lpConsoleTitle);
 
-    static void f() {
-    }
+    static void f() {}
 }
